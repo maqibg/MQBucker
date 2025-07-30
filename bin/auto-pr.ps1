2e33ce469b42b000912d9a80c1d94662b02019b5
马乞 <38286162+maqibg@users.noreply.github.com>
2025-07-30 17:10:48 +0800

    Update auto-pr.ps1

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -7,3 +7,4 @@ if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
 $autopr = "$env:SCOOP_HOME/bin/auto-pr.ps1"
 $dir = "$PSScriptRoot/../bucket" # checks the parent dir
 & $autopr -Dir $dir -Upstream $Upstream @Args
+
